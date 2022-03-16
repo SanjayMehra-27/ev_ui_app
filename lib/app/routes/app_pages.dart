d696adeec1efa71e1e1493e713794f0d18b3d795
Sanjay Mehra <mehra271198@gmail.com>
2022-03-16 13:02:32 +0530

    login page route added

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,14 +1,19 @@
 import 'package:ev_ui_app/app/modules/home/view/home_vew.dart';
 import 'package:ev_ui_app/app/modules/insights/view/insights_vew.dart';
+import 'package:ev_ui_app/app/modules/login/view/login_view.dart';
 import 'package:ev_ui_app/app/modules/settings/view/settings_view.dart';
 import 'package:ev_ui_app/app/modules/supports/view/supports_view.dart';
 import 'package:ev_ui_app/app/modules/usage_history/view/usage_history_vew.dart';
 import 'package:ev_ui_app/app/routes/app_routes.dart';
 import 'package:get/get.dart';
 class AppPages {
-  static const INITIAL = AppRoutes.HOME;
+  static const INITIAL = AppRoutes.LOGIN;
 
   static final routes = [
+    GetPage(
+      name: AppRoutes.LOGIN,
+      page: () => LoginPage(),
+    ),
     GetPage(
       name: AppRoutes.HOME,
       page: () => const HomeView(),
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,4 +5,5 @@ class AppRoutes {
   static const SUPPORTS = '/supports';
   static const SETTINGS = '/settings';
   static const PAGE_NOT_FOUND = '/page-not-found';
+  static const LOGIN = '/login';
 }
