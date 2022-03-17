ee10204013a668b534980ce97d0839434482bf7a
Sanjay Mehra <mehra271198@gmail.com>
2022-03-17 14:00:15 +0530

    fixed build issue

--- lib/app/modules/forgot_password/view/forgot_password_view.dart
+++ lib/app/modules/forgot_password/view/forgot_password_view.dart
@@ -15,50 +15,48 @@ class ForgotPasswordPage extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Expanded(
-            child: Column(
-              children: [
-                // ignore: todo
-                // TODO: App Image LOGO
-                const LoginHeaderWidget(),
-                  
-                const SizedBox(height: 50),
-                // ignore: todo
-                // TODO: User Email & Password Input Fields & Forgot Password Button, Reset Password Button Respectively
-                Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: 'Email',
-                          labelStyle: Heading1BoldBlack,
-                          fillColor: SECONDARY_COLOR.withOpacity(0.15),
-                          filled: true,
-                          border: InputBorder.none),
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: Get.width,
-                      height: 45,
-                      child: ElevatedButton(
-                        style:
-                            ElevatedButton.styleFrom(primary: PRIMARY_COLOR),
-                        onPressed: () {},
-                        child: Text(
-                          'Send Forgot Password Link',
-                          style: Heading1White,
-                        ),
+          child: Column(
+            children: [
+              // ignore: todo
+              // TODO: App Image LOGO
+              const LoginHeaderWidget(),
+                
+              const SizedBox(height: 50),
+              // ignore: todo
+              // TODO: User Email & Password Input Fields & Forgot Password Button, Reset Password Button Respectively
+              Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Email',
+                        labelStyle: Heading1BoldBlack,
+                        fillColor: SECONDARY_COLOR.withOpacity(0.15),
+                        filled: true,
+                        border: InputBorder.none),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: Get.width,
+                    height: 45,
+                    child: ElevatedButton(
+                      style:
+                          ElevatedButton.styleFrom(primary: PRIMARY_COLOR),
+                      onPressed: () {},
+                      child: Text(
+                        'Send Forgot Password Link',
+                        style: Heading1White,
                       ),
-                      // ignore: todo
-                      // TODO: Sign in button
                     ),
-                  ],
-                ),
-            
-                // ignore: todo
-                // TODO: Sponsored by [Image]
-                const SponsoredByWidget(),
-              ],
-            ),
+                    // ignore: todo
+                    // TODO: Sign in button
+                  ),
+                ],
+              ),
+          
+              // ignore: todo
+              // TODO: Sponsored by [Image]
+              const SponsoredByWidget(),
+            ],
           ),
         ),
       ),
--- lib/app/modules/login/view/login_view.dart
+++ lib/app/modules/login/view/login_view.dart
@@ -17,108 +17,111 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Expanded(
-            child: Column(
-              children: [
-                // TODO: App Image LOGO
-                const LoginHeaderWidget(),
-            
-                const SizedBox(height: 50),
-                // TODO: User Email & Password Input Fields & Forgot Password Button, Reset Password Button Respectively
-                 Column(
-                   crossAxisAlignment: CrossAxisAlignment.end,
-                   children: [
-                     TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Email',
-                        labelStyle: Heading1BoldBlack,
-                        fillColor: SECONDARY_COLOR.withOpacity(0.15),
-                        filled: true,
-                        border: InputBorder.none
-                      ),
-                     ),
-                 TextButton(onPressed: (){
-                  try {
-                     Get.toNamed('/forgot-password'); 
-                  } catch (e) {
-                    print(e);
-                  }                  
-                 }, child: const Text(
-                   'Forgot Password ?',
-                   textAlign: TextAlign.end,
-                    style: TextStyle(color: SECONDARY_COLOR),)),
-                   ],
-                 ),
-                 
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Container(
-                        width: Get.width,
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            hintText: 'Password',
-                            labelStyle: Heading1BoldBlack,
-                            fillColor: SECONDARY_COLOR.withOpacity(0.15),
-                            filled: true,
-                            border: InputBorder.none
-                          ),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Reset ?',
-                          textAlign: TextAlign.end,
-                          style: TextStyle(color: SECONDARY_COLOR),
-                        )),
-                    ],
-                  ),
-                    
-                // TODO: Sign in button
-                SizedBox(
-                  width: Get.width,
-                  height: 45,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: PRIMARY_COLOR
+          child: Column(
+            children: [
+              // ignore: todo
+              // TODO: App Image LOGO
+              const LoginHeaderWidget(),
+          
+              const SizedBox(height: 50),
+              // ignore: todo
+              // TODO: User Email & Password Input Fields & Forgot Password Button, Reset Password Button Respectively
+               Column(
+                 crossAxisAlignment: CrossAxisAlignment.end,
+                 children: [
+                   TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      labelStyle: Heading1BoldBlack,
+                      fillColor: SECONDARY_COLOR.withOpacity(0.15),
+                      filled: true,
+                      border: InputBorder.none
                     ),
-                    onPressed: (){
-                  }, child: Text(
-                    'Sign In',
-                    style: Heading1White,
-                  ),),
-                ),
-            
-                // TODO: Register Page Link
-                const SizedBox(height: 20),
-                RichText(
-                  text: TextSpan(
-                    text: 'Don\'t have an account? ',
-                    style: const TextStyle(color: Colors.black),
-                    children: [
-                      TextSpan(
-                        mouseCursor: MouseCursor.uncontrolled,
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Get.toNamed('/home');
-                          },
-                        text: 'Register',
-                        style: const TextStyle(
-                          color: DARK_COLOR,
-                          fontWeight: FontWeight.bold,
+                   ),
+               TextButton(onPressed: (){
+                try {
+                   Get.toNamed('/forgot-password'); 
+                } catch (e) {
+                  print(e);
+                }                  
+               }, child: const Text(
+                 'Forgot Password ?',
+                 textAlign: TextAlign.end,
+                  style: TextStyle(color: SECONDARY_COLOR),)),
+                 ],
+               ),
+               
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      width: Get.width,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          labelStyle: Heading1BoldBlack,
+                          fillColor: SECONDARY_COLOR.withOpacity(0.15),
+                          filled: true,
+                          border: InputBorder.none
                         ),
                       ),
-                      const TextSpan(text: ' here.'),
-                    ],
-                  ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Reset ?',
+                        textAlign: TextAlign.end,
+                        style: TextStyle(color: SECONDARY_COLOR),
+                      )),
+                  ],
                 ),
-            
-                // TODO: Sponsored by [Image]
-                const SponsoredByWidget(),
-              ],
-            ),
+                  
+              // ignore: todo
+              // TODO: Sign in button
+              SizedBox(
+                width: Get.width,
+                height: 45,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: PRIMARY_COLOR
+                  ),
+                  onPressed: (){
+                }, child: Text(
+                  'Sign In',
+                  style: Heading1White,
+                ),),
+              ),
+          
+              // ignore: todo
+              // TODO: Register Page Link
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Don\'t have an account? ',
+                  style: const TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(
+                      mouseCursor: MouseCursor.uncontrolled,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Get.toNamed('/home');
+                        },
+                      text: 'Register',
+                      style: const TextStyle(
+                        color: DARK_COLOR,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const TextSpan(text: ' here.'),
+                  ],
+                ),
+              ),
+          
+              // ignore: todo
+              // TODO: Sponsored by [Image]
+              const SponsoredByWidget(),
+            ],
           ),
         ),
       ),
